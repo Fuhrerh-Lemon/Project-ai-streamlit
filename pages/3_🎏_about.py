6db6a724207900f1d0fba821681abc9d8c67e867
Fuhrerh Lemon <lemon.repositorios@outlook.com>
2022-10-20 22:25:08 -0500

    auto-commit {fuhrerhlemon@fedora, jue 20 oct 2022 22:25:08 EST}

--- pages/3_🎏_about.py
+++ pages/3_🎏_about.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from info import authors
-st.header('TEAM')
-authors.authors()
 
+st.header('EQUIPO')
+
+made_fuhrerhlemon = authors.made_by_user("FuhrerhLemon", "https://github.com/Fuhrerh-Lemon")
+made_fuhrerhlemon.made_by()
+made_fuhrerhlemon.info_dev()
 st.markdown("")
